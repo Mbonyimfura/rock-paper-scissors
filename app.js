db160c42c708330c23637093d360e9bad2085aaf
Mbonyimfura <mbonyimfuratresor@gmail.com>
2023-07-14 16:30:06 +0200

    Add ui

--- app.js
+++ app.js
@@ -1,72 +1,86 @@
-const getComputerChoice=()=>{
-   const choices=['rock','paper','scissors']
-   const randomIndex=Math.floor(Math.random()*choices.length);
-   return choices[randomIndex]
+const buttons = document.querySelectorAll("button");
+const content = document.createElement("div");
+const container = document.querySelector(".container");
 
-}
+let playerScore = 0;
+let computerScore = 0;
+
+const getComputerChoice = () => {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+};
 
 //function to play single round
- const playRound=(playerSelection,computerSelection)=>{
-playerSelection=playerSelection.toLowerCase();
+const playRound = (playerSelection, computerSelection) => {
+  playerSelection = playerSelection.toLowerCase();
 
-if(playerSelection===computerSelection){
-    return 'Its a tie'
-}
-if(playerSelection==='rock'){
-    if(computerSelection==='paper'){
-        return 'You lose! Paper beats rock!'
-    }else{
-        return 'You win! Rocks beats scissors!'
+  if (playerSelection === computerSelection) {
+   return
+  }
+  if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      computerScore++;
+      return 
+    } else {
+      playerScore++;
+      return 
     }
-}
-if(playerSelection==='paper'){
-    if(computerSelection==='scissors'){
-        return 'You lose! Scissors beats paper'
-    }else{
-        return 'You win! Paper beats rock!'
+  }
+  if (playerSelection === "paper") {
+    if (computerSelection === "scissors") {
+      computerScore++;
+      return 
+    } else {
+      playerScore++;
+      return 
     }
-}
-if(playerSelection==='scissors'){
-    if(computerSelection==='rock'){
-        return 'You lose!rock beats scissors'
-    }else{
-        return 'You win!scissors beats paper!'
+  }
+  if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      computerScore++;
+      return 
+    } else {
+      playerScore++;
+      return 
     }
-}
-return 'Invalid choice! Please choose rock,paper,scissors'
+  }
+  return "Invalid choice! Please choose rock,paper,scissors";
+};
 
-   }
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const playerSelection = button.id;
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+    displayResult(result);
+    displayScore();
+    checkWinner();
+  });
+});
 
-   //function to play 5 round
-   const game=()=>{
-    let playerScore=0;
-    let computerScore=0;
-    for(let round=1;round<=5;round++){
-      const  playerSelection=prompt('enter your choice(rock,paper,scissors):')
-     const  computerSelection=getComputerChoice()
-      console.log(`round ${round}:`)  
-      console.log(`player chooses ${playerSelection}:`)
-      console.log(`computer chooses ${computerSelection}:`)
-
-      const result=playRound(playerSelection,computerSelection);
-      console.log(result)
-
-      if(result.startsWith('You win')){
-        playerScore++
-      }else if(result.startsWith('You lose')){
-        computerScore++
-      }
-    }
-console.log('Game over!');
-console.log(`Player score:${playerScore}`)
-console.log(`Computer score:${computerScore}`)
-if(playerScore>computerScore){
-    console.log(`You win the game`)
-}else if(playerScore<computerScore){
-    console.log(`Computer win the game`)
-}else{
-    console.log(`It's a tie`)
-}
-   }
-   //start the game
-   game()
+const displayResult = (result) => {
+  content.textContent = result;
+  container.appendChild(content);
+};
+// function to display score
+const displayScore = () => {
+  const scoreElement = document.createElement("p");
+  scoreElement.textContent = `Player: ${playerScore} score - computer: ${computerScore} score`;
+  container.appendChild(scoreElement);
+};
+//function to check the winner of the game
+const checkWinner = () => {
+  if (playerScore === 5) {
+    displayResult("Congratulation! You win the game");
+    disableButtons();
+  } else if (computerScore === 5) {
+    displayResult("Computer wins the game!");
+    disableButtons();
+  } 
+};
+const disableButtons = () => {
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
+};
